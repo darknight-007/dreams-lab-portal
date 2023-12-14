cbd70ced7b6ec250728afaa67323ca499ac1d2eb
darknight-007 <jdas5@asu.edu>
2023-12-14 00:04:23 +0000

    initial commit

--- DeepGIS/models.py
+++
@@ -1,9 +0,0 @@
-# deepgis/models.py
-
-from django.contrib.gis.db import models
-
-class TiledGISInfo(models.Model):
-    name = models.CharField(max_length=200)
-    geo_data = models.PolygonField()
-    zoom_level = models.IntegerField()
-    tile_data = models.BinaryField()  # Store tile binary data or use a FileField for file paths
